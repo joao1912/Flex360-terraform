1d6795aac9214711ee14c96a45d003ccdbf24748
joao <joaopedrot1912@gmail.com>
2024-08-06 20:50:19 -0300

    feat: adicionado grupos de segurança relacionados entre si, banco de dados na subnete-priv-3

---
+++ database.tf
@@ -0,0 +1,55 @@
+
+resource "aws_db_subnet_group" "rds-subnet-group" {
+
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.subnet-private-3.id]
+
+}
+
+resource "aws_security_group_rule" "ingress-rds-rule" {
+
+  to_port                  = 5432
+  from_port                = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg-db.id
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.sg-ec2-flex360.id
+
+}
+
+resource "aws_security_group_rule" "egress-rds-rule" {
+
+  to_port           = 0
+  from_port         = 0
+  protocol          = -1
+  security_group_id = aws_security_group.sg-db.id
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+resource "aws_security_group" "sg-db" {
+
+}
+
+resource "aws_db_instance" "database-flex360" {
+
+  identifier           = "database-flex360"
+  engine               = "postgres"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "password123"
+  parameter_group_name = "default.postgres13"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  publicly_accessible  = false
+
+  backup_retention_period = 7
+  multi_az                = false
+
+  availability_zone      = aws_subnet.subnet-private-3.id
+  db_name                = "flex360"
+  vpc_security_group_ids = [aws_security_group.sg-db.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
+
+}
--- launchTemplate.tf
+++ launchTemplate.tf
@@ -1,12 +1,44 @@
+resource "aws_security_group_rule" "ingress-ec2-flex360" {
+
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  type                     = "ingress"
+  security_group_id        = aws_security_group.sg-ec2-flex360.id
+  source_security_group_id = aws_security_group.SG-alb.id
+
+}
+
+resource "aws_security_group_rule" "egress-ec2-flex360" {
+
+  protocol          = -1
+  from_port         = 0
+  to_port           = 0
+  type              = "egress"
+  security_group_id = aws_security_group.sg-ec2-flex360.id
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+resource "aws_security_group" "sg-ec2-flex360" {
+
+  name        = "ec2-flex360"
+  description = "Acesso http via alb"
+  vpc_id      = aws_vpc.flex360-vpc.id
+
+}
 
 resource "aws_launch_template" "template-flex360" {
   name_prefix   = "template-flex360-"
   image_id      = var.ami
   instance_type = var.instance_type
 
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.sg-ec2-flex360.id]
+  }
+
   user_data = filebase64("${path.module}/user_data.sh")
-  
-  
 
   lifecycle {
     create_before_destroy = true
