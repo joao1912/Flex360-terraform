f8992efd6fd91f1cbdded719e08df0e61a10ea97
joao <joaopedrot1912@gmail.com>
2024-08-12 15:17:05 -0300

    feat: cache redis

---
+++ elastiCache.tf
@@ -0,0 +1,18 @@
+
+resource "aws_elasticache_subnet_group" "subnetes_caches" {
+  name       = "subnetes_privates_cache"
+  subnet_ids = [aws_subnet.subnet-private-3.id, aws_subnet.subnet-private-4.id]
+
+}
+
+resource "aws_elasticache_cluster" "cache_cluster" {
+
+  cluster_id           = "redis-cluster"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  subnet_group_name    = aws_elasticache_subnet_group.subnetes_caches.name
+  port                 = 6379
+
+}
