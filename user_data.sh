b95fb1f96e33bfad2c53e3791e32ef4fc70480bd
Joao Pedro <joaopedrot1912@gmail.com>
2024-09-24 10:57:47 -0300

    feat: recursos adicionados no user_data

--- user_data.sh
+++ user_data.sh
@@ -10,3 +10,16 @@ sudo usermod -aG docker $USER
 sudo systemctl start docker
 sudo systemctl enable docker
 
+mkdir -p /config/env
+
+# executar comando para buscar as envs no secret maneger
+
+echo "DB_URL=valor" >> /config/env/.env
+echo "DB_USERNAME=valor" >> /config/env/.env
+echo "DB_PASSWORD=valor" >> /config/env/.env
+echo "PORT=80" >> /config/env/.env
+echo "BUCKET=valor" >> /config/env/.env
+
+docker pull joaopedrot1912/flex360:latest
+
+docker run -d -p 80:80 -v /config/env/.env:/app/.env joaopedrot1912/flex360-api
