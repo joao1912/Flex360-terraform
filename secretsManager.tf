ed8d2dead82251b34a1cbee1621f5435032bbbb4
Joao Pedro <joaopedrot1912@gmail.com>
2024-10-18 00:21:18 -0300

    refactor: arquivo userData e secretsManager

--- secretsManager.tf
+++ secretsManager.tf
@@ -7,10 +7,10 @@ resource "aws_secretsmanager_secret_version" "secrets_db" {
   depends_on   = [aws_db_instance.database-flex360]
   secret_id    = aws_secretsmanager_secret.db_host.id
   secret_string = jsonencode({
-    host     = aws_db_instance.database-flex360.address
-    username = "nome-teste"
-    password = "senha123"
-    port     = 0000
+    DB_HOST     = aws_db_instance.database-flex360.address
+    DB_USERNAME = "nome-teste"
+    DB_PASSWORD = "senha123"
+    DB_PORT     = aws_db_instance.database-flex360.port
   })
 }
 
@@ -24,18 +24,5 @@ resource "aws_secretsmanager_secret_version" "secret_alb_dns" {
   secret_id    = aws_secretsmanager_secret.alb_dns.id
   secret_string = jsonencode({
     dns_name = aws_lb.alb-flex360.dns_name
-  })
-}
-
-resource "aws_secretsmanager_secret" "secret_s3_name" {
-  name        = "s3_name"
-  description = "Nome do bucket do s3"
-}
-
-resource "aws_secretsmanager_secret_version" "secret_s3_name_version" {
-  depends_on   = [aws_s3_bucket.bucket_images]
-  secret_id    = aws_secretsmanager_secret.secret_s3_name.id
-  secret_string = jsonencode({
-    bucket_name = aws_s3_bucket.bucket_images.bucket
   })
 }
--- user_data.sh
+++ user_data.sh
@@ -12,13 +12,31 @@ sudo systemctl enable docker
 
 mkdir -p /config/env
 
-# executar comando para buscar as envs no secret maneger
+PATH_ENV="/config/env/.env"
 
-echo "DB_URL=valor" >> /config/env/.env
-echo "DB_USERNAME=valor" >> /config/env/.env
-echo "DB_PASSWORD=valor" >> /config/env/.env
-echo "PORT=80" >> /config/env/.env
-echo "BUCKET=valor" >> /config/env/.env
+DB_DATA=$(aws secretsmanager get-secret-value --secret-id secrets_database --query "SecretString" --output text | jq -r '. | to_entries[] | "\(.key)=\(.value)"')
+
+HTTP_ORIGIN=$(aws ssm get-parameter --name front-flex360-origin --query "Parameter.Value" --output text)
+
+BUCKET_NAME=$(aws ssm get-parameter --name bucket-flex360 --query "Parameter.Value" --output text)
+
+SECRET_KEY=$(aws secretsmanager get-secret-value --secret-id secret_key_token --query "SecretString" --output text | jq -r '.secret_key')
+
+DB_NAME="flex-database"
+DB_PORT=$(echo "$DB_DATA" | grep '^DB_PORT=' | cut -d '=' -f2)
+DB_HOST=$(echo "$DB_DATA" | grep '^DB_HOST=' | cut -d '=' -f2)
+
+echo "$DB_DATA" > "$PATH_ENV"
+
+echo "DB_URL=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME" >> "$PATH_ENV"
+
+echo "BUCKET=$BUCKET_NAME" >> /config/env/.env
+
+echo "HTTP_ORIGIN=$HTTP_ORIGIN" >> /config/env/.env
+
+echo "SECRET_KEY=$SECRET_KEY" >> /config/env/.env
+
+echo "PROFILE=prod" >> /config/env/.env
 
 docker pull joaopedrot1912/flex360:latest
 
