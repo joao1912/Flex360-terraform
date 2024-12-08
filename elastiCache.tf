4ca26be9d771552747aac41e6d05083b79b3ff25
Joao Pedro <joaopedrot1912@gmail.com>
2024-12-08 14:18:32 -0300

    feat: grupo de seegurança adicionado no cache

--- elastiCache.tf
+++ elastiCache.tf
@@ -1,3 +1,26 @@
+resource "aws_security_group" "SG-cache" {
+  vpc_id = aws_vpc.flex360-vpc.id
+  name   = "SG-cache"
+}
+
+resource "aws_security_group_rule" "access_cache_ingress" {
+  type              = "ingress"
+  source_security_group_id = aws_security_group.sg-ec2-flex360.id
+  from_port         = 6379
+  to_port           = 6379
+  protocol          = "tcp"
+  security_group_id = aws_security_group.SG-cache.id
+}
+
+resource "aws_security_group_rule" "access_cache_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.SG-cache.id
+}
+
 resource "aws_elasticache_subnet_group" "subnetes_caches" {
   name       = "subnetes-caches"
   subnet_ids = [aws_subnet.subnet-private-3.id, aws_subnet.subnet-private-4.id]
@@ -17,4 +40,5 @@ resource "aws_elasticache_cluster" "cache_cluster" {
   parameter_group_name = aws_elasticache_parameter_group.redis7_parameter_group.name
   subnet_group_name    = aws_elasticache_subnet_group.subnetes_caches.name
   port                 = 6379
+  security_group_ids = [ aws_security_group.SG-cache.id ]
 }
